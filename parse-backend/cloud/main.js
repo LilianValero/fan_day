23bb5082d1f2dbc97bae355481acb2a4979cbc31
Harri Lehtola <peruukki@hotmail.com>
2014-04-02 23:33:39 +0300

    Simple Tapatalk test function

--- parse-backend/cloud/main.js
+++ parse-backend/cloud/main.js
@@ -1,6 +1,23 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("tapatalk", function(request, response) {
+  testTapatalk(response);
 });
+
+function testTapatalk(response) {
+  Parse.Cloud.httpRequest({
+    method: 'POST',
+    url: 'https://support.tapatalk.com/mobiquo/mobiquo.php',
+    headers: {
+      'Content-Type': 'text/xml'
+    },
+    body: '<?xml version="1.0"?><methodCall><methodName>get_config</methodName><params></params></methodCall>',
+    success: function(httpResponse) {
+      response.success(httpResponse.text);
+    },
+    error: function(httpResponse) {
+      response.error('Request failed with response ' + httpResponse.status + ' ' + httpResponse.text);
+    }
+  });
+}
