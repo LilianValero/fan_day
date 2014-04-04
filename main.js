3d85ddcf80f40a0b3b69c646a5fa28baabbe703a
Harri Lehtola <peruukki@hotmail.com>
2014-04-05 00:10:31 +0300

    Get forum information if available

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 var xmlrpc = require('xmlrpc');
 
 var client = xmlrpc.createClient('http://www.tottenhamhotspurs.tv/forum/mobiquo/mobiquo.php');
-client.methodCall('get_config', [], function(err, res) {
-  if (err) console.log('Error', err)
-  else console.log('Success', res);
+client.methodCall('get_config', [], function(err, config) {
+  if (err) console.log('Error in get_config', err);
+  else {
+    if (!config.get_forum) console.log('No forum available');
+    else {
+      client.methodCall('get_forum', [true], function(err, forum) {
+        if (err) console.log('Error in get_forum', err);
+        else console.log('Forum', forum);
+      });
+    }
+  }
 });
