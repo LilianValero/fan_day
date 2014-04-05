aabc2a70a189c6e157f78a62ce8a01df3560792d
Harri Lehtola <peruukki@hotmail.com>
2014-04-05 14:20:59 +0300

    Retrieve sub forum information up to Match Day Chat

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
+'use strict';
+
 var xmlrpc = require('xmlrpc');
+var _ = require('underscore');
 
 var client = xmlrpc.createClient('http://www.tottenhamhotspurs.tv/forum/mobiquo/mobiquo.php');
-client.methodCall('get_config', [], function(err, config) {
-  if (err) console.log('Error in get_config', err);
+getConfig();
+
+function getConfig() {
+  callMethod('get_config', getForum);
+}
+
+function getForum(config) {
+  if (config.get_forum) callMethod('get_forum', getSubForum);
+  else console.log('No forum available');
+}
+
+function getSubForum(forum) {
+  var toDareIsToDoSubForum = _.find(forum, function(subForum) { return subForum.forum_id === '6' });
+  if (!toDareIsToDoSubForum) console.log('Could not find To Dare Is To Do ! sub-forum');
   else {
-    if (!config.get_forum) console.log('No forum available');
+    console.log('getSubForum', toDareIsToDoSubForum);
+    var matchDayChatSubForum = _.find(toDareIsToDoSubForum.child, function(subForum) { return subForum.forum_id === '81' });
+    if (!matchDayChatSubForum) console.log('Could not find Match Day Chat sub-forum');
     else {
-      client.methodCall('get_forum', [true], function(err, forum) {
-        if (err) console.log('Error in get_forum', err);
-        else console.log('Forum', forum);
-      });
+      console.log('TODO: Get topics with get_topic using forum ID ' + matchDayChatSubForum.forum_id);
     }
   }
-});
+}
+
+function callMethod(method, callback) {
+  client.methodCall(method, [], function(err, response) {
+    if (err) console.log('Error in ' + method, err);
+    else {
+      console.log('Response to ' + method, response);
+      callback(response);
+    }
+  });
+}
