fbed3c6f36c7ab4eb1be4a21e67f5fe76af63ded
Harri Lehtola <peruukki@hotmail.com>
2014-04-23 23:37:40 +0300

    Add Compass support

--- appgyver_angular_json/Gruntfile.js
+++ appgyver_angular_json/Gruntfile.js
@@ -10,7 +10,25 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-steroids");
+  grunt.loadNpmTasks("grunt-contrib-compass");
 
-  grunt.registerTask("default", ["steroids-make", "steroids-compile-sass"]);
+  grunt.registerTask("steroids-compass", "Compile sass files from the stylesheets and put them in the dist/stylesheets directory", function() {
+    grunt.extendConfig({
+      compass: {
+        options: {
+          sassDir: 'www/stylesheets',
+          cssDir: 'dist/stylesheets',
+          imagesDir: 'www/images',
+          fontsDir: 'www/fonts',
+          httpImagesPath: '/images',
+          relativeAssets: false,
+          debugInfo: false
+        },
+        dis: {}
+      }
+    });
+    return grunt.task.run("compass");
+  });
 
+  grunt.registerTask("default", ["steroids-make", "steroids-compass"]);
 };
