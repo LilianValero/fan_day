fd87220fd3970e861a1ea2e4ce3dfb32019df3ca
Harri Lehtola <peruukki@hotmail.com>
2014-04-27 14:44:41 +0300

    Add 'homeTeam' field to combined posts.json

--- combine.js
+++ combine.js
@@ -6,7 +6,14 @@ var fs = require('fs');
 var arsenalPosts = require('./arsenalPosts.json');
 var tottenhamPosts = require('./tottenhamPosts.json');
 
-var allPosts = arsenalPosts.concat(tottenhamPosts);
+var extendedArsenalPosts = _.map(arsenalPosts, _.partial(extendPostInfo, false));
+var extendedTottenhamPosts = _.map(tottenhamPosts, _.partial(extendPostInfo, true));
+
+function extendPostInfo(isHomeTeam, postInfo) {
+  return _.extend(postInfo, { homeTeam: isHomeTeam });
+}
+
+var allPosts = extendedArsenalPosts.concat(extendedTottenhamPosts);
 var allPostsSorted = _.sortBy(allPosts, function(postInfo) { return postInfo.timestamp; });
 
 var allPostsFileName = './posts.json';
