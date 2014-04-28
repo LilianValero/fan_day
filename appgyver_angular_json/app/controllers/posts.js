e4622019ea03fe545c1a9d4c77d96b8fdd2b0c07
alex <alexandre.fourticq@stonesoft.com>
2014-04-28 18:58:35 +0200

    - date formatting   - html content binding   - filter that preview 300 characters

--- appgyver_angular_json/app/controllers/posts.js
+++ appgyver_angular_json/app/controllers/posts.js
@@ -9,3 +9,21 @@ postsApp.controller('IndexCtrl', function($scope, PostsRestangular) {
   // -- Native navigation
   steroids.view.navigationBar.show("TOT 0 - 1 ARS posts");
 });
+
+postsApp.filter('preview', function () {
+    return function (text, length, end) {
+        if (isNaN(length))
+            length = 10;
+
+        if (end === undefined)
+            end = "...";
+
+        if (text.length <= length || text.length - end.length <= length) {
+            return text;
+        }
+        else {
+            return String(text).substring(0, length-end.length) + end;
+        }
+
+    };
+});
